957f691ef925126f3d05b3af6a3fde16654eda28
Stefan Majewsky <stefan.majewsky@sap.com>
2021-04-09 13:46:27 +0200

    add test coverage for ParseHelm2Manifest

---
+++ helm-manifest-parser/parser_test.go
@@ -0,0 +1,64 @@
+/*******************************************************************************
+*
+* Copyright 2021 SAP SE
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You should have received a copy of the License along with this
+* program. If not, you may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestParseManifests(t *testing.T) {
+	testCases := []struct {
+		InputPath  string
+		OutputPath string
+		Parser     func([]byte) (string, error)
+	}{
+		{"fixtures/helm-v2-manifest.dat", "fixtures/helm-v2-parsed.json", ParseHelm2Manifest},
+	}
+
+	for _, tc := range testCases {
+		inBytes, err := ioutil.ReadFile(tc.InputPath)
+		must(t, err)
+		outStr, err := tc.Parser(bytes.TrimSpace(inBytes))
+		must(t, err)
+
+		//in order to diff(1) with `tc.OutputPath`, we need to apply the same indentation
+		var outBuf bytes.Buffer
+		must(t, json.Indent(&outBuf, []byte(outStr), "", "  "))
+		outBuf.WriteString("\n")
+		must(t, ioutil.WriteFile(tc.OutputPath+".actual", outBuf.Bytes(), 0666))
+
+		cmd := exec.Command("diff", "-u", tc.OutputPath+".actual", tc.OutputPath)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		must(t, cmd.Run())
+	}
+}
+
+func must(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
