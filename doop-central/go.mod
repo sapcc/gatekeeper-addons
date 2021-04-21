a50acae8bb4a26a01d678c9cab97a2b437cd48dc
Stefan Majewsky <stefan.majewsky@sap.com>
2021-04-21 12:21:21 +0200

    doop-central: setup OpenStack/Swift client

--- doop-central/go.mod
+++ doop-central/go.mod
@@ -2,4 +2,10 @@ module github.com/sapcc/gatekeeper-addons/doop-central
 
 go 1.16
 
-require github.com/sapcc/go-bits v0.0.0-20210326161745-782f1d0cfdee
+require (
+	github.com/gophercloud/gophercloud v0.15.1-0.20210202035223-633d73521055
+	github.com/gophercloud/utils v0.0.0-20210323225332-7b186010c04f
+	github.com/jpillora/longestcommon v0.0.0-20161227235612-adb9d91ee629 // indirect
+	github.com/majewsky/schwift v0.0.0-20201014122716-bd1c74594efe
+	github.com/sapcc/go-bits v0.0.0-20210326161745-782f1d0cfdee
+)
--- doop-central/main.go
+++ doop-central/main.go
@@ -24,6 +24,10 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack"
+	"github.com/gophercloud/utils/openstack/clientconfig"
+	"github.com/majewsky/schwift/gopherschwift"
 	"github.com/sapcc/go-bits/httpee"
 	"github.com/sapcc/go-bits/logg"
 )
@@ -33,17 +37,47 @@ func main() {
 		logg.Fatal("usage: %s <listen-address>", os.Args[0])
 	}
 
+	//initialize OpenStack/Swift client
+	provider, err := clientconfig.AuthenticatedClient(nil)
+	must("initialize OpenStack client", err)
+	client, err := openstack.NewObjectStorageV1(provider, gophercloud.EndpointOpts{})
+	must("initialize Swift client", err)
+	account, err := gopherschwift.Wrap(client, &gopherschwift.Options{
+		UserAgent: "doop-agent/rolling",
+	})
+	must("initialize Swift account", err)
+	swiftContainer, err := account.Container(mustGetenv("REPORT_CONTAINER_NAME")).EnsureExists()
+	must("initialize Swift container", err)
+
+	//collect HTTP handlers
 	mux := http.NewServeMux()
 	mux.HandleFunc("/healthcheck", handleHealthcheck)
 
+	//start HTTP server
 	handler := logg.Middleware{}.Wrap(mux)
-
 	logg.Info("listening on " + os.Args[1])
 	ctx := httpee.ContextWithSIGINT(context.Background())
-	err := httpee.ListenAndServeContext(ctx, os.Args[1], handler)
+	err = httpee.ListenAndServeContext(ctx, os.Args[1], handler)
 	if err != nil {
 		logg.Fatal(err.Error())
 	}
+
+	//TODO use
+	_ = swiftContainer
+}
+
+func must(task string, err error) {
+	if err != nil {
+		logg.Fatal("could not %s: %s", task, err.Error())
+	}
+}
+
+func mustGetenv(key string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		logg.Fatal("missing required environment variable: " + key)
+	}
+	return val
 }
 
 func handleHealthcheck(w http.ResponseWriter, r *http.Request) {
