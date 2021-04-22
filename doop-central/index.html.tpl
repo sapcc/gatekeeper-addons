64586851db0dd491d2b72966bfc31b25566095d6
Stefan Majewsky <stefan.majewsky@sap.com>
2021-04-22 16:23:37 +0200

    doop-central: cluster health report

--- doop-central/index.html.tpl
+++ doop-central/index.html.tpl
@@ -1,11 +1,41 @@
 <!DOCTYPE html>
-<link rel="preload" href="/static/style.css" as="style">
+<link rel="stylesheet" href="/static/style.css">
 <h1>
   Decentralized Observer Of Policies
   <small><a href="https://futurama.fandom.com/wiki/Democratic_Order_of_Planets">[huh?]</a></small>
 </h1>
-{{ range $cluster, $report := . }}
-<h2>{{ $cluster }}</h2>
-<pre><code>{{ jsonIndent $report }}</code></pre>
-{{ end }}
-<link rel="stylesheet" href="/static/style.css" preload>
+
+<h2>Cluster health</h2>
+
+<table>
+  <thead>
+    {{- range $clusterGroup := $.AllClusterGroups }}
+      <tr>
+        <th class="nobr">&nbsp;</th>
+        {{- range $cluster := index $.ClustersByGroup $clusterGroup }}
+          <th class="nobr"><div>{{ $cluster }}</div></th>
+        {{- end }}
+      </tr>
+      <tr>
+        <th class="nobr">Oldest data</th>
+        {{- range $cluster := index $.ClustersByGroup $clusterGroup }}
+          {{- $info := index $.ClusterInfos $cluster }}
+          <td class="nobr center {{ $info.OldestAuditCSSClass }}">{{ printf "%.1fs" $info.OldestAuditAgeSecs }}</th>
+        {{- end }}
+      </tr>
+      <tr>
+        <th class="nobr">Newest data</th>
+        {{- range $cluster := index $.ClustersByGroup $clusterGroup }}
+          {{- $info := index $.ClusterInfos $cluster }}
+          <td class="nobr center {{ $info.NewestAuditCSSClass }}">{{ printf "%.1fs" $info.NewestAuditAgeSecs }}</th>
+        {{- end }}
+      </tr>
+    {{- end }}
+  </thead>
+</table>
+
+<!-- TODO: remove debug display -->
+{{- range $cluster := $.AllClusters }}
+  <h2>Raw report: {{ $cluster }}</h2>
+  <pre><code>{{ index $.Reports $cluster | jsonIndent }}</code></pre>
+{{- end }}
--- doop-central/ui.go
+++ doop-central/ui.go
@@ -23,8 +23,12 @@ import (
 	"bytes"
 	_ "embed"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"sort"
+	"strings"
 	"text/template"
+	"time"
 
 	"github.com/sapcc/go-bits/logg"
 )
@@ -38,6 +42,9 @@ var (
 		"jsonIndent": jsonIndent,
 	}
 )
+
+////////////////////////////////////////////////////////////////////////////////
+// type UI
 
 //UI provides the business logic for rendering the web dashboard.
 type UI struct {
@@ -57,11 +64,44 @@ func (ui UI) RenderMainPage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//TODO preprocess `reports` before passing it into the template
+	data := struct {
+		AllClusters      []string
+		AllClusterGroups []string
+		ClustersByGroup  map[string][]string
+		ClusterInfos     map[string]clusterInfo
+		Reports          map[string][]byte //TODO remove (only used for debug display)
+	}{
+		ClustersByGroup: make(map[string][]string),
+		ClusterInfos:    make(map[string]clusterInfo),
+		Reports:         reports,
+	}
+
+	for clusterName, reportBytes := range reports {
+		var r report
+		err := json.Unmarshal(reportBytes, &r)
+		if err != nil {
+			msg := fmt.Sprintf("cannot parse report for %s: %s", clusterName, err.Error())
+			http.Error(w, msg, http.StatusInternalServerError)
+			return
+		}
+
+		data.AllClusters = append(data.AllClusters, clusterName)
+		clusterGroup := clusterGroupOf(clusterName)
+		data.ClustersByGroup[clusterGroup] = append(data.ClustersByGroup[clusterGroup], clusterName)
+		data.ClusterInfos[clusterName] = r.ToClusterInfo()
+	}
+
+	sort.Strings(data.AllClusters)
+	for clusterGroup, clusterNames := range data.ClustersByGroup {
+		sort.Strings(clusterNames)
+		data.AllClusterGroups = append(data.AllClusterGroups, clusterGroup)
+	}
+	sort.Strings(data.AllClusterGroups)
+
 	w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline'")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	err = pageTemplate.Execute(w, reports)
+	err = pageTemplate.Execute(w, data)
 	if err != nil {
 		logg.Error("while rendering index.html: %s", err.Error())
 	}
@@ -71,4 +111,75 @@ func jsonIndent(in []byte) string {
 	var buf bytes.Buffer
 	json.Indent(&buf, in, "", "  ")
 	return buf.String()
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// report datatypes and structured data for HTML template
+
+func clusterGroupOf(clusterName string) string {
+	for _, prefix := range []string{"a-", "k-", "s-", "v-"} {
+		if strings.HasPrefix(clusterName, prefix) {
+			return prefix
+		}
+	}
+	return ""
+}
+
+type report struct {
+	Templates []templateReport `json:"templates"`
+}
+
+type templateReport struct {
+	Kind    string         `json:"kind"`
+	Configs []configReport `json:"configs"`
+}
+
+type configReport struct {
+	Name       string            `json:"name"`
+	AuditAt    time.Time         `json:"auditTimestamp"`
+	Violations []violationReport `json:"violations"`
+}
+
+type violationReport struct {
+	Kind      string `json:"kind"`
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+	Message   string `json:"message"`
+}
+
+type clusterInfo struct {
+	OldestAuditAgeSecs  float64
+	OldestAuditCSSClass string
+	NewestAuditAgeSecs  float64
+	NewestAuditCSSClass string
+}
+
+func (r report) ToClusterInfo() clusterInfo {
+	now := time.Now()
+	var info clusterInfo
+	for _, rt := range r.Templates {
+		for _, rc := range rt.Configs {
+			auditAgeSecs := now.Sub(rc.AuditAt).Seconds()
+			if info.OldestAuditAgeSecs == 0 || info.OldestAuditAgeSecs < auditAgeSecs {
+				info.OldestAuditAgeSecs = auditAgeSecs
+			}
+			if info.NewestAuditAgeSecs == 0 || info.NewestAuditAgeSecs > auditAgeSecs {
+				info.NewestAuditAgeSecs = auditAgeSecs
+			}
+		}
+	}
+
+	info.OldestAuditCSSClass = cssClassForAge(info.OldestAuditAgeSecs)
+	info.NewestAuditCSSClass = cssClassForAge(info.NewestAuditAgeSecs)
+	return info
+}
+
+func cssClassForAge(ageSecs float64) string {
+	if ageSecs >= 900 {
+		return "value-danger"
+	}
+	if ageSecs >= 300 {
+		return "value-warning"
+	}
+	return "value-ok"
 }
