4a05360dcbcd8e7820ce84cc9607a7d0d1d2ab64
Stefan Majewsky <stefan.majewsky@sap.com>
2023-09-04 15:31:58 +0200

    doop-api: add scaffold, download reports
    Most of this is directly lifted from doop-central.

--- doop-api/main.go
+++ doop-api/main.go
@@ -19,5 +19,57 @@
 
 package main
 
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack"
+	"github.com/gophercloud/utils/openstack/clientconfig"
+	"github.com/majewsky/schwift/gopherschwift"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sapcc/go-api-declarations/bininfo"
+	"github.com/sapcc/go-bits/httpapi"
+	"github.com/sapcc/go-bits/httpapi/pprofapi"
+	"github.com/sapcc/go-bits/httpext"
+	"github.com/sapcc/go-bits/logg"
+	"github.com/sapcc/go-bits/must"
+	"github.com/sapcc/go-bits/osext"
+	"go.uber.org/automaxprocs/maxprocs"
+)
+
 func main() {
+	logg.ShowDebug = osext.GetenvBool("DOOP_API_DEBUG")
+	undoMaxprocs := must.Return(maxprocs.Set(maxprocs.Logger(logg.Debug)))
+	defer undoMaxprocs()
+
+	wrap := httpext.WrapTransport(&http.DefaultTransport)
+	wrap.SetOverrideUserAgent(bininfo.Component(), bininfo.VersionOr("rolling"))
+
+	//initialize OpenStack/Swift client
+	ao := must.Return(clientconfig.AuthOptions(nil))
+	ao.AllowReauth = true
+	provider := must.Return(openstack.NewClient(ao.IdentityEndpoint))
+	must.Succeed(openstack.Authenticate(provider, *ao))
+	client := must.Return(openstack.NewObjectStorageV1(provider, gophercloud.EndpointOpts{}))
+	account := must.Return(gopherschwift.Wrap(client, nil))
+	containerName := osext.MustGetenv("DOOP_API_SWIFT_CONTAINER")
+	container := must.Return(account.Container(containerName).EnsureExists())
+	downloader := NewDownloader(container)
+
+	//collect HTTP handlers
+	handler := httpapi.Compose(
+		API{downloader},
+		httpapi.HealthCheckAPI{SkipRequestLog: true},
+		pprofapi.API{IsAuthorized: pprofapi.IsRequestFromLocalhost},
+	)
+	mux := http.NewServeMux()
+	mux.Handle("/", handler)
+	mux.Handle("/metrics", promhttp.Handler()) //TODO custom metrics
+
+	//start HTTP server
+	ctx := httpext.ContextWithSIGINT(context.Background(), 10*time.Second)
+	listenAddress := osext.GetenvOrDefault("DOOP_API_LISTEN_ADDRESS", ":8080")
+	must.Succeed(httpext.ListenAndServeContext(ctx, listenAddress, mux))
 }
