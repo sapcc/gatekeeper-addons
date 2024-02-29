10ae1975f6ad019221f4180b544cf7bb486046bd
Sandro Jäckel <sandro.jaeckel@sap.com>
2024-02-29 16:49:09 +0100

    Don't let the gzip handler copy the entire writer

--- doop-api/api.go
+++ doop-api/api.go
@@ -20,11 +20,14 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/NYTimes/gziphandler"
 	"github.com/gorilla/mux"
 	"github.com/sapcc/go-bits/httpapi"
+	"github.com/sapcc/go-bits/logg"
 	"github.com/sapcc/go-bits/respondwith"
 )
 
@@ -38,6 +41,29 @@ func (a API) AddTo(r *mux.Router) {
 	r.Methods("GET").Path("/v2/violations").Handler(gziphandler.GzipHandler(http.HandlerFunc(a.handleGetViolations)))
 }
 
+// The Gzip middleware will use the first few writes to decide whether to use compression or not
+// because it stores the first write in its internal buffer, we must ensure that it doesn't grow too much.
+type chunkingWriter struct {
+	inner io.Writer
+}
+
+func (w chunkingWriter) Write(buf []byte) (int, error) {
+	chunkSize := 1048576 // 1 MiB
+	written := 0
+
+	for len(buf) > chunkSize {
+		n, err := w.inner.Write(buf[0:chunkSize])
+		written += n
+		buf = buf[chunkSize:]
+		if err != nil {
+			return written, err
+		}
+	}
+
+	n, err := w.inner.Write(buf)
+	return written + n, err
+}
+
 func (a API) handleGetViolations(w http.ResponseWriter, r *http.Request) {
 	httpapi.IdentifyEndpoint(r, "/v2/violations")
 
@@ -47,5 +73,14 @@ func (a API) handleGetViolations(w http.ResponseWriter, r *http.Request) {
 	}
 	result := AggregateReports(reports, BuildFilterSet(r.URL.Query()))
 	result.Sort()
-	respondwith.JSON(w, http.StatusOK, result)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	encoder := json.NewEncoder(chunkingWriter{inner: w})
+	err = encoder.Encode(&result)
+	if err != nil {
+		logg.Error("while encoding violations json: %w", err)
+		return
+	}
 }
